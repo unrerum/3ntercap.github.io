53e588038576b3feb3d814531a835d516d800409
Andrey <50292360+3ntercap@users.noreply.github.com>
2022-10-26 19:42:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    css3: true,
+    scrollingSpeed: 1500,
+    autoScrolling: true,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+});
